97cd2b38f7d7d7876fc855a059bdedb61a46536d
Irfan <irfnhm@gmail.com>
2018-03-23 18:28:19 +0530

    Removed hardcoded values and strings. Th design is almost done.

--- app/src/main/java/com/example/irfan/quizapp/MainActivity.java
+++ app/src/main/java/com/example/irfan/quizapp/MainActivity.java
@@ -11,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private void questonOneAnswer(){}
 
+    //Submit button
+    private void submitGame(){}
+
+    //Reset Button
+    private void resetGame(){
+        
+    }
 }
